56b47bbd73c7d5ff102911f20630b5438ddaa4e0
Yuri.R <44mmnrw@gmail.com>
2026-01-16 18:01:57 +0300

    Исправлена загрузка lottie-player в head для работы анимации на проде

--- wp-content/themes/arsenal/functions.php
+++ wp-content/themes/arsenal/functions.php
@@ -344,13 +344,7 @@ if ( ! function_exists( 'arsenal_enqueue_scripts' ) ) {
 			'https://unpkg.com/@lottiefiles/lottie-player@latest',
 			array(),
 			'1.0',
-			true
-		);
-
-		// Контроль интервала проигрывания Lottie анимаций
-		wp_enqueue_script(
-			'lottie-player-interval',
-			ARSENAL_THEME_URI . '/assets/js/lottie-player-interval.js',
+		false  // В head, чтобы загрузилось раньше
 			array( 'lottie-player' ),
 			ARSENAL_VERSION,
 			true
